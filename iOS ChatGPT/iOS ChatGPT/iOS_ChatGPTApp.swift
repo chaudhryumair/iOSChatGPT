97740f33369897c3580908b1f0569f88eab9b533
chaudhryumair <chaudhryumair007@yahoo.com>
2025-06-02 02:10:02 +0500

    Project Created

---
+++ iOS ChatGPT/iOS ChatGPT/iOS_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ChatGPTApp.swift
+//  iOS ChatGPT
+//
+//  Created by Chaudhry Umair on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS ChatGPT/iOS ChatGPTTests/iOS_ChatGPTTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ChatGPTTests.swift
+//  iOS ChatGPTTests
+//
+//  Created by Chaudhry Umair on 02/06/2025.
+//
+
+import Testing
+@testable import iOS_ChatGPT
+
+struct iOS_ChatGPTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
